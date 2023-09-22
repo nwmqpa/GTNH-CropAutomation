7ff421f71e9eb02f2f7e6eaed6877bd839b9c69c
DylanTaylor1 <89085119+DylanTaylor1@users.noreply.github.com>
2023-09-22 18:19:40 -0400

    Rename install to setup

---
+++ setup.lua
@@ -0,0 +1,36 @@
+local shell = require('shell')
+local args = {...}
+local branch
+local repo
+local scripts = {
+    'action.lua',
+    'database.lua',
+    'gps.lua',
+    'posUtil.lua',
+    'scanner.lua',
+    'signal.lua',
+    'autoStat.lua',
+    'autoTier.lua',
+    'autoSpread.lua',
+    'config.lua',
+    'uninstall.lua'
+}
+
+-- BRANCH
+if #args >= 1 then
+    branch = args[1]
+else
+    branch = 'main'
+end
+
+-- REPO
+if #args >= 2 then
+    repo = args[2]
+else
+    repo = 'https://raw.githubusercontent.com/DylanTaylor1/ic2-crop-automation/'
+end
+
+-- INSTALL
+for i=1, #scripts do
+    shell.execute(string.format('wget -f %s%s/%s', repo, branch, scripts[i]))
+end
--- uninstall.lua
+++ uninstall.lua
@@ -10,7 +10,7 @@ local scripts = {
     'autoTier.lua',
     'autoSpread.lua',
     'config.lua',
-    'install.lua',
+    'setup.lua',
     'uninstall.lua'
 }
 
