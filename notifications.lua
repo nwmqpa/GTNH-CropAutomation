ac92cfd56597aa1e4b6c015b0a000c2ebd23ad24
Thomas Nicollet <thomas.nicollet@nebulis.io>
2024-05-08 20:59:28 +0200

    :bug: Fixed issue with notifications

--- notifications.lua
+++ notifications.lua
@@ -1,16 +1,9 @@
 local config = require('config')
-local internet = require("internet")
+local internet = require('internet')
 
 local function sendNotification(title, content)
     if config.ntfyEnabled then
-        internet.request(
-            "https://ntfy.sh/" .. config.ntfyChannel,
-            content,
-            {
-                "Title" = title,
-                "Tags" = "shamrock"
-            }
-        )
+        internet.request("https://ntfy.sh/" .. config.ntfyChannel, content, { Title = title, Tags = 'shamrock' })
     end
     print(title .. ": " .. content)
 end
